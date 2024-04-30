34f83964e7eaa239e7d21b350e5c017892305fce
HP <sabit7130@yahoo.com>
2024-04-30 23:07:38 +0200

    2d commit

--- src/test/java/seleniumtestcases/addlead.java
+++ src/test/java/seleniumtestcases/addlead.java
@@ -5,8 +5,10 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
@@ -39,6 +41,17 @@ public class addlead {
 
         // الضغط على زر إضافة ليد
         driver.findElement(By.xpath("//body/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/a[1]")).click();
+        driver.findElement(By.xpath("//body/div[1]/div[3]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]/input[1]")).sendKeys("ahmed");
+        driver.findElement(By.xpath("//body/div[1]/div[3]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys("0102188765439");
+        WebElement dropdownElement = driver.findElement(By.xpath("//body/div[1]/div[3]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[1]/div[3]/div[1]/select[1]")); 
+        Select dropdown = new Select(dropdownElement);
+        dropdown.selectByVisibleText("olx"); 
+        WebElement dropdownElement1 = driver.findElement(By.xpath("//*[@id=\"add_lead\"]/div/div/div[2]/form/div[1]/div[8]/div/select"));
+        Select dropdown1 = new Select(dropdownElement1);
+        dropdown1.selectByIndex(0);
+        driver.findElement(By.xpath("//body/div[1]/div[3]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[1]/div[9]/div[1]/textarea[1]")).sendKeys("notes");
+        driver.findElement(By.xpath("//body/div[1]/div[3]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[2]/button[1]")).click();
+      
     }
 
   
