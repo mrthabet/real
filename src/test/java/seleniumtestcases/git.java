73507610af7a4622dda16df9f24292d63aeade4c
Ahmed thabet <156729011+mrthabet@users.noreply.github.com>
2024-07-03 05:10:11 +0300

    second commit

---
+++ src/test/java/seleniumtestcases/git.java
@@ -0,0 +1,39 @@
+package seleniumtestcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import selenium.login;
+
+public class git {
+
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+    	//
+    	WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testOrangeHRM() throws IOException { 
+        login login = new login(driver);
+        login.login("admin@comp1.com", "123456789"); 
+
+       
+    }
+
+ 
+   // @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
